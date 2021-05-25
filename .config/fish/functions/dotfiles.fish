cf6d81dbc5940f7c4be65d967542a36b4f689961
Saúl Nogueras <susensio@nuc>
2021-05-25 08:16:09 +0200

    Add default dotfiles status

--- .config/fish/functions/dotfiles.fish
+++ .config/fish/functions/dotfiles.fish
@@ -1,7 +1,9 @@
 function dotfiles --description 'Dotfiles repository'
     alias _dotfiles '/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME'
 
-    if test $argv[1] = 'cmp'
+    if test (count $argv) -eq 0
+        _dotfiles status
+	else if test "$argv[1]" = 'cmp'
         _dotfiles add -u
         _dotfiles commit -m $argv[2]
         _dotfiles push
