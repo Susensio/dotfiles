740b22aaa7131a465450c7b28218e018d4ac814d
Susensio <susensio@notebook>
2022-03-05 20:19:31 +0100

    Fix install script

--- .github/install.sh
+++ .github/install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+shopt -s expand_aliases
 alias _dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
 git clone --bare https://github.com/Susensio/dotfiles.git $HOME/.dotfiles.git/
 _dotfiles config --local status.showUntrackedFiles no
@@ -8,9 +9,21 @@ if [ $? = 0 ]; then
   echo "Checked out dotfiles.";
 else
   echo "Backing up pre-existing dot files...";
-  mkdir -p .dotfiles-backup
-  _dotfiles checkout 2>&1 | egrep "^\s+" | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+  backupdir="$HOME/.dotfiles-backup/"
+  echo $backupdir
+  mkdir -p $backupdir
+  files=`_dotfiles checkout 2>&1 | egrep "^\s+" | awk {'print $1'}`
+  for file in $files; do
+    mkdir -p "$backupdir$(dirname $file)"
+    mv $file "$backupdir$file"
+  done
+
   _dotfiles checkout
-  echo "Checked out dotfiles.";
+  if [ $? = 0 ]; then
+    echo "Checked out dotfiles.";
+  else
+    echo "Error, repository cannot be cloned"
+  fi;
 fi;
+
 unalias _dotfiles
