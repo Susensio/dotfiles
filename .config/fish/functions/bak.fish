551d9c9fac517ade0f079e1954f9298f7ec15720
Saúl Nogueras <susensio@nuc>
2022-11-15 11:15:55 +0100

    Fix bak fish function to do folders

--- .config/fish/functions/bak.fish
+++ .config/fish/functions/bak.fish
@@ -1,6 +1,8 @@
 # By Moritz Kneilmann | github.com/MoritzKn
-function bak --desc "Adds the sufix '.bak' (backup) to files"
-    for file in $argv
-        cp $file $file.bak
+function bak --desc "Adds the sufix '.bak' (backup) to files and folders"
+    for arg in $argv
+		# remove trailing slash if folder
+		set file (string replace -r '(.)/$' '$1' -- $arg)
+        cp -r $file $file.bak
     end
 end
